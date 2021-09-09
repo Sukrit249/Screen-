f6fc61a67fb89fb7345827c8116b75b1ff901c7d
Somrak <Sukritspek1@gmail.com>
2021-09-09 08:45:40 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,25 @@
 
 <template>
   <div id="app">
-  <!-- Hier codes Du HTML-->
-  
   <header id="title">
     {{title}}
   </header>
       <div id="currentDates">  
         {{currentDate}}
       </div>
-   <div id="entry-body">
-      <ul id="body-list">
-        <li class="entry-list" v-for="entry in filteredEntries" :key="entry.id">
-        <span class="entry-daytime"><strong class="entry-daytime">{{entry[0]}}Uhr, {{entry[1].replace("/", ".")}} </strong></span>
-        <h3 class="entry-title">{{entry[2]}}</h3>
-        <span class="entry-description">{{entry[3]}}</span>
+   <div class="entry-body">
+      <ul id="body-list" >
+        <li class="entry-list" v-for="entry in filteredEntries" :key="entry.id" >
+          <span class="entry-daytime"><strong class="entry-daytime">{{entry[0]}}Uhr, {{entry[1].replace("/", ".")}} </strong></span>
+          <h3 class="entry-title">{{entry[2]}}</h3>
+          <span class="entry-description">{{entry[3]}}</span>
         </li>
       </ul>
     </div>
     <div id="entry-footers">
-        <img id="STZH" src="./assets/STZH_SEB_Logo.jpg">
-          <!-- alt="Opportunity Logo -->
-        <img id="SAG" src="./assets/SAG_Logo_De.jpg">
-          <!-- alt="SAG Logo" -->
-        <img id="Opportunity" src="./assets/Opportunity.jpg">
-          <!-- alt=" Stadt Zürich logo" -->  
+      <img id="STZH" src="./assets/STZH_SEB_Logo.jpg" alt="Opportunity Logo">
+      <img id="SAG" src="./assets/SAG_Logo_De.jpg" alt="Stiftung SAG Logo">
+      <img id="Opportunity" src="./assets/Opportunity.jpg" alt=" Stadt Zürich logo">
     </div> 
   </div>
 </template>
@@ -40,8 +35,7 @@ export default {
       entries: [],
       currentDate:"",
      }
-    },   
-  /*Function*/
+    }, /*Function*/
  computed: { 
    //computet properties are like data properties, but with a methods combined
     filteredEntries(){
@@ -81,7 +75,6 @@ export default {
   background: #e8eff4;
 }
 /* #app { 
-
 } */
 #title{
   font-family: 'Inter', sans-serif;
@@ -100,7 +93,7 @@ export default {
   opacity: 0.5;
   color:black;
 }
-#entry-body{
+.entry-body{
 padding: 1vw;  
 }
 .entry-title{
@@ -138,12 +131,27 @@ padding: 1vw;
   color: rgb(245, 210, 157);
   list-style: none;
 }
+.nothing{
+  width: 3.35vw;
+  height: 7.5vw;
+  padding: 1vw;
+  padding-left: 5vw;
+  font-size: 6vw;
+  font-family: Inter;
+  opacity: 0.5;
+  color:black;
+  padding: 2vw;
+  margin: 2.5vw 0;
+  background: #0f05a0;
+  font-size: 3vw;
+  line-height: 1.3;
+  font-family: 'Inter', sans-serif;
+}
 #entry-footers{ 
   left: 0;
   bottom: 0;
   width: 100vw;
   background: white;
-  
   position: fixed;
   clear:both;
   display: flex;
